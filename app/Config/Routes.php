d935ae4c071cc3d22c5642bb1ef1138e91c6b1ec
mylinuxtests-lgtm <mylinuxtests@gmail.com>
2025-10-20 15:03:41 -0600

    Add files via upload

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,8 +10,8 @@ $routes->get('/', 'Login::index');
 $routes->post('Login/validar_usuario', 'Login::validar_usuario');
 $routes->get('Inicio', 'Comisiones::index');
 $routes->get('Comisiones', 'Comisiones::index');
-$routes->get('Login/cerrar','Login::cerrar');
-$routes->get('Catalogos/listado_estructura','Catalogos::listado_estructura');
+$routes->get('Login/cerrar', 'Login::cerrar');
+$routes->get('Catalogos/listado_estructura', 'Catalogos::listado_estructura');
 $routes->post('Catalogos/getEstructura', 'Catalogos::getEstructura');
 $routes->get('Comisiones/agregar', 'Comisiones::agregar');
 $routes->post('Comisiones/guardar', 'Comisiones::guardar');
@@ -21,6 +21,15 @@ $routes->get('Comisiones/exportar_csv', 'Comisiones::exportar_csv');
 $routes->post('getcomision', 'Getcomision::index');
 $routes->get('Comisiones/exportar_csv/(:num)', 'Comisiones::exportar_csv/$1');
 $routes->get('index.php', 'Comisiones::index');
+$routes->get('Comisiones/descargar_csv_temp/(:any)', 'Comisiones::descargar_csv_temp/$1');
+$routes->get('Catalogos/listado_puestos', 'Catalogos::listado_puestos');
+$routes->post('Catalogos/getPuestos', 'Catalogos::getPuestos');
+$routes->post('Catalogos/guardaPuesto', 'Catalogos::guardaPuesto');
+$routes->post('Catalogos/eliminarPuesto', 'Catalogos::eliminarPuesto');
+$routes->get('Catalogos/listado_tipo_contrato', 'Catalogos::listado_tipo_contrato');
+$routes->post('Catalogos/getTipoContrato', 'Catalogos::getTipoContrato');
+$routes->post('Catalogos/guardaTipoContrato', 'Catalogos::guardaTipoContrato');
+$routes->post('Catalogos/eliminarTipoContrato', 'Catalogos::eliminarTipoContrato');
 
 
 /* $routes->get('Agregar/(:any)','Forms::index/$1',['filter' => 'perfil:1,3']);
